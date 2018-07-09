0d35480dd35e5fe59da12056cbf18fdfabd5e70f
Wendy Parsons <wsparsons@gmail.com>
2018-07-09 00:03:03 -0700

    create account crud

--- src/controllers/accounts.js
+++ src/controllers/accounts.js
@@ -8,4 +8,68 @@ function getAll(req, res, next){
 
 function getOne(req, res, next){
   const id = req.params.id
+  const data = model.getOne(id)
+  if(!data){
+    return next({
+      status: 404,
+      message: `Could not find account with ID of ${id}`
+    })
+  }
+}
+
+function create(req, res, next){
+  const { name, bankName, description } = req.body
+  if(!name || !bankName || !description){
+    return next({
+      status: 400,
+      message: `Name, bank name and description are required`
+    })
+  }
+  const data = model.create(name, bankName, description)
+  if(!data){
+    return next({
+      status: 404,
+      message: `Could not create new account`
+    })
+  }
+  res.status(201).json({ data })
+}
+
+function update(req, res, next){
+  const id = req.params.id
+  const { name, bankName, description } = req.body
+  if(!name || !bankName || !description){
+    return next({
+      status: 400,
+      message: `Name, bank name and description are required`
+    })
+  }
+  const data = model.update(id, name, bankName, description)
+  if(!data){
+    return next({
+      status: 404,
+      message: `Could not update due to wrong account ID of ${id}`
+    })
+  }
+  res.status(200).json({ data })
+}
+
+function remove(req, res, next){
+  const id = req.params.id
+  const data = model.remove(id)
+  if(!data){
+    return next({
+      status: 404,
+      message: `Could not delete due to wrong account ID of ${id}`
+    })
+  }
+  res.status(200).json({ data })
+}
+
+module.exports = {
+  getAll,
+  getOne,
+  create,
+  update,
+  remove
 }
--- src/models/accounts.js
+++ src/models/accounts.js
@@ -1,1 +1,40 @@
 // MODELS ACCOUNTS
+const uuid = require('uuid/v4')
+const accounts = require('./data')
+
+function getAll(){
+  return accounts
+}
+
+function getOne(){
+  return accounts.find(account => account.id === id)
+}
+
+function create(name, bankName, description){
+  const account = { id: uuid(), name, bankName, description}
+  accounts.push(account)
+  return account
+}
+
+function update(id, name, bankName, description){
+  const account = accounts.find(account => account.id === id)
+  account.name = name
+  account.bankName = bankName
+  account.description = description
+  return account
+}
+
+function remove(id){
+  const account = accounts.find(account => account.id === id)
+  const index = accounts.indexOf(account)
+  accounts.splice(index, 1)
+  return account
+}
+
+module.exports = {
+  getAll,
+  getOne,
+  create,
+  update,
+  remove
+}
